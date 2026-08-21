8b0f85f10a463bb31c0110ad6cd6eb5236501f90
Cid Queiroz <cydy.queiroz@gmail.com>
2026-08-21 18:33:21 -0300

    feat: add type declarations for @cidqueiroz/cdkteck-ui components in vite-env.d.ts

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,31 @@
 /// <reference types="vite/client" />
+
+declare module '@cidqueiroz/cdkteck-ui' {
+  export const ThemeProvider: any;
+  export const AppShell: any;
+  export const Header: any;
+  export const CDKHeader: any;
+  export const CDKShell: any;
+  export const CDKLoginPage: any;
+  export const LoginPage: any;
+  export const CDKMetricCard: any;
+  export const CDKButton: any;
+  export const CDKCard: any;
+  export const CDKTable: any;
+  export const CDKModal: any;
+  export const CDKSelect: any;
+  export const CDKInput: any;
+  export const CDKBadge: any;
+  export const CDKTabs: any;
+  export const CDKSkeleton: any;
+  export const CDKAvatar: any;
+  export const CDKDropdown: any;
+  export const CDKPill: any;
+  export const CDKProgressBar: any;
+  export const CDKStatCard: any;
+  export const CDKContainer: any;
+  export const CDKSpinner: any;
+  export const theme: any;
+  const content: any;
+  export default content;
+}
